29fc7907945c2afed54ba90584a80bf05f4beb6d
aMinecart <ben.frankson@icloud.com>
2025-03-28 00:36:04 -0400

    Created camera to follow player (not functional)

--- PlayerScripts/PlayerMovement.cs
+++ PlayerScripts/PlayerMovement.cs
@@ -3,14 +3,12 @@ using Godot;
 
 public partial class PlayerMovement : CharacterBody2D
 {
-	[Export]
-	public int Speed { get; set; } = 400;
+    private Vector2 InputDirection;
 
-	private Vector2 inputDirection;
+    [Export]
+	public float Speed { get; set; } = 400.0f;
 
-	private float maxSpeed = 40*10.0f; // maximum speed the player can move the X or Y direction
-
-	private float acceleration = 5.0f;
+	private float Acceleration = 5.0f;
 
 	private float friction = 40*4.0f; // amount the player slows down by each second (doubled when the player's desired move direction is equal to Vector2.zero) 
 
@@ -19,7 +17,7 @@ public partial class PlayerMovement : CharacterBody2D
 		return 2 * f / (f * f + 1);
 	}
 
-	private Vector2 accelerate(Vector2 currVel, Vector2 accelDir, float accelRate, float maxVel, float friction, float fixedDeltaTime)
+	private Vector2 accelerate(Vector2 currVel, Vector2 accelDir, float accelRate, float maxVel, float fixedDeltaTime)
 	{
 		if (accelDir == Vector2.Zero)
 		{
@@ -29,47 +27,18 @@ public partial class PlayerMovement : CharacterBody2D
 		// multiplying the acceleration by the max velocity makes it so that the player accelerates up to max velocity in the same amount of time, not matter how big it is
 		float accelX = accelDir.X * accelRate * maxVel * fixedDeltaTime;
 
-		// alt calculation
 		if (Mathf.Sign(currVel.X) == Mathf.Sign(accelDir.X))
 		{
-			// print("slowing (X)");
-
 			// float speedToMaxSpeedRatio = Mathf.Clamp01((maxVel - Mathf.Abs(currVel.X)) / maxVel);
-			accelX *= calcRampup(Mathf.Clamp(((maxVel - Mathf.Abs(currVel.X)) / maxVel), 0, 1)); // lower acceleration as current X speed gets closer to max speed
+			accelX *= calcRampup(Mathf.Clamp((maxVel - Mathf.Abs(currVel.X)) / maxVel, 0, 1)); // lower acceleration as current X speed gets closer to max speed
 		}
-
-		/* alt 2
-		if (Mathf.Abs(currVel.X) > maxVel && Mathf.Sign(currVel.X) == Mathf.Sign(accelDir.X))
-		{
-			// print("stopping accel (X)");
-			accelX = accelDir.X * friction * Time.deltaTime;
-		}
-		else if (Mathf.Abs(currVel.X) > maxVel / 2 && Mathf.Sign(currVel.X) == Mathf.Sign(accelDir.X)) // lower acceleration as current X speed gets closer to max speed
-		{
-			// print("slowing accel (X)");
-			accelX /= 2;
-		}
-		 */
-
-		// original calculation
-		/*
-		if (Mathf.Abs(currVel.X) > maxVel / 2 && Mathf.Sign(currVel.X) == Mathf.Sign(accelDir.X))
-		{
-			// print("slowing (X)");
-			accelX *= Mathf.Clamp01((maxVel - Mathf.Abs(currVel.X) + 0.5f) / maxVel); // lower acceleration as current X speed gets closer to max speed
-		}
-		 */
-
-		// print(accelX);
 
 		float accelY = accelDir.Y * accelRate * maxVel * fixedDeltaTime;
 
 		if (Mathf.Sign(currVel.Y) == Mathf.Sign(accelDir.Y))
 		{
-			// print("slowing (Y)");
-			accelY *= calcRampup(Mathf.Clamp(((maxVel - Mathf.Abs(currVel.Y)) / maxVel), 0, 1)); // lower acceleration as current Y speed gets closer to max speed
+			accelY *= calcRampup(Mathf.Clamp((maxVel - Mathf.Abs(currVel.Y)) / maxVel, 0, 1)); // lower acceleration as current Y speed gets closer to max speed
 		}
-		// print(accelY);
 
 		return currVel + new Vector2(accelX, accelY);
 	}
@@ -100,7 +69,7 @@ public partial class PlayerMovement : CharacterBody2D
 
 	public void GetInput()
 	{
-		inputDirection = Input.GetVector("left", "right", "up", "down");
+		InputDirection = Input.GetVector("left", "right", "up", "down");
 	}
 	
 	public void UpdateGlobalVars()
@@ -110,8 +79,8 @@ public partial class PlayerMovement : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		Velocity = applyFriction(Velocity, inputDirection, friction, (float)delta);
-		Velocity = accelerate(Velocity, inputDirection, acceleration, maxSpeed, friction, (float)delta);
+		Velocity = applyFriction(Velocity, InputDirection, friction, (float)delta);
+		Velocity = accelerate(Velocity, InputDirection, Acceleration, Speed, (float)delta);
 
 		GetInput();
 		MoveAndSlide();
