28efb957de64a943f9785c3e75e528f0d6727f30
minecart <ben.frankson@icloud.com>
2025-05-12 10:15:09 -0400

    Re-apply changes from "dungeon-(with-collision)"

--- Scripts/EnemyScripts/EnemyTurretScript.cs
+++ Scripts/EnemyScripts/EnemyTurretScript.cs
@@ -5,19 +5,27 @@ public partial class EnemyTurretScript : Node2D
 {
 	[Export] public PackedScene BulletScene { get; set; }
 	private CharacterBody2D enemyBody;
+	private Node2D enemyBulletParent;
+	
 	private void Shoot()
 	{
 		Node2D bullet = (Node2D)BulletScene.Instantiate();
-        bullet.GlobalPosition = GlobalPosition;
+		bullet.GlobalPosition = GlobalPosition;
 		bullet.Rotation = enemyBody.Rotation;
-		GetTree().GetRoot().GetNode("TestScene/EnemyBulletParent").AddChild(bullet);
+		enemyBulletParent.AddChild(bullet);
 	}
 
-    public override void _Ready()
-    {
-        base._Ready();
+	public override void _Ready()
+	{
+		base._Ready();
 		enemyBody = GetNode<CharacterBody2D>("CharacterBody2D");
-    }
+		
+		enemyBulletParent = GetTree().GetRoot().GetNode<Node2D>("Dungeon/EnemyBulletParent");
+		if (enemyBulletParent is null)
+		{
+			enemyBulletParent = GetTree().GetRoot().GetNode<Node2D>("TestScene/EnemyBulletParent");
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
@@ -30,10 +38,10 @@ public partial class EnemyTurretScript : Node2D
 	}
 
 	private void _OnArea2DAreaEntered(Node2D area)
-    {
-        if (area.IsInGroup("playerAttack"))
-        {
-            CallDeferred(MethodName.Free);
-        }
-    }
+	{
+		if (area.IsInGroup("playerAttack"))
+		{
+			CallDeferred(MethodName.Free);
+		}
+	}
 }
